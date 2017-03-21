24cb24f5d02e6b5a8b124e9b79ff928ae6a1001c
Marsh, Edward <edward.marsh@sky.uk>
2017-03-21 15:53:04 +0000

    type printer

--- test.go
+++ test.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var x = 20.0
+
+	y := 42
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
 }
