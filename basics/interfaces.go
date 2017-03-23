a255a16bad76e395817d9ceb29763f19c6d9f4d6
Marsh, Edward <edward.marsh@sky.uk>
2017-03-23 15:07:04 +0000

    Add override for say hi to grad

--- basics/interfaces.go
+++ basics/interfaces.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+type Human struct {
+  name string
+  age int
+  phone int
+}
+
+type Student struct {
+  Human
+  university string
+  course string
+}
+
+type Grad struct {
+  Human
+  job string
+}
+
+func (h *Human) SayHi() {
+  fmt.Println("Hi! My name is %s\n", h.name)
+}
+
+func (h *Human) GiveAge() {
+  fmt.Println("I am %s\n" h.age)
+}
+
+func (h *Human) GiveNumber() {
+  fmt.Println("My phone number is %s\n", h.phone)
+}
+
+func (g *Grad) SayHi() {
+  fmt.Println("Hi! My name is %s\n", g.name, "I am a grad")
+}
