44b10e3fff0253b37b0f12b55afd5147d7534a40
Marsh, Edward <edward.marsh@sky.uk>
2017-03-23 16:20:25 +0000

    Add employed boolean

--- basics/interfaces.go
+++ basics/interfaces.go
@@ -7,6 +7,7 @@ import (
 type User struct {
 	FirstName, LastName string
 	Age                 int
+	Employed            bool
 }
 
 func (u *User) GetFirstName() string {
@@ -21,9 +22,14 @@ func (u *User) GetAge() int {
 	return u.Age
 }
 
+func (u *User) GetEmployed() bool {
+	return u.Employed
+}
+
 func main() {
-	u := &User{"John", "Smith", 30}
+	u := &User{"John", "Smith", 30, true}
 	fmt.Println(u.GetFirstName())
 	fmt.Println(u.GetLastName())
 	fmt.Println(u.GetAge())
+	fmt.Println(u.GetEmployed())
 }
