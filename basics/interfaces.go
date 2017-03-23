b90688ee5c41aecf8d2b1aa31fa499fd203413f5
Marsh, Edward <edward.marsh@sky.uk>
2017-03-23 15:07:48 +0000

    Fix syntax error

--- basics/interfaces.go
+++ basics/interfaces.go
@@ -3,34 +3,34 @@ package main
 import "fmt"
 
 type Human struct {
-  name string
-  age int
-  phone int
+	name  string
+	age   int
+	phone int
 }
 
 type Student struct {
-  Human
-  university string
-  course string
+	Human
+	university string
+	course     string
 }
 
 type Grad struct {
-  Human
-  job string
+	Human
+	job string
 }
 
 func (h *Human) SayHi() {
-  fmt.Println("Hi! My name is %s\n", h.name)
+	fmt.Println("Hi! My name is %s\n", h.name)
 }
 
 func (h *Human) GiveAge() {
-  fmt.Println("I am %s\n" h.age)
+	fmt.Println("I am %s\n", h.age)
 }
 
 func (h *Human) GiveNumber() {
-  fmt.Println("My phone number is %s\n", h.phone)
+	fmt.Println("My phone number is %s\n", h.phone)
 }
 
 func (g *Grad) SayHi() {
-  fmt.Println("Hi! My name is %s\n", g.name, "I am a grad")
+	fmt.Println("Hi! My name is %s\n", g.name, "I am a grad")
 }
