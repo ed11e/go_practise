b6ec348bdbf78d8204d03cb0e1ca1e7c7366016d
Marsh, Edward <edward.marsh@sky.uk>
2017-04-19 09:59:11 +0100

    Add new files

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var templates *template.Template
+
+func main() {
+	var allFiles []string
+	files, err := ioutil.ReadDir("./templates")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range files {
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".tmpl") {
+			allFiles = append(allFiles, "./templates/"+filename)
+		}
+	}
+
+	templates, err = template.ParseFiles(allFiles...)
+
+	s1 := templates.Lookup("header.tmpl")
+	s1.ExecuteTemplate(os.Stdout, "header", nil)
+	fmt.Println()
+	s2 := templates.Lookup("content.tmpl")
+	s2.ExecuteTemplate(os.Stdout, "content", nil)
+	fmt.Println()
+	s3 := templates.Lookup("footer.tmpl")
+	s3.ExecuteTemplate(os.Stdout, "footer", nil)
+	fmt.Println()
+	s3.Execute(os.Stdout, nil)
+}
