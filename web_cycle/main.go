361889605845bb57181866ef9bde5d7d489a25f5
Marsh, Edward <edward.marsh@sky.uk>
2017-04-26 10:44:33 +0100

    Refactor to use urls

--- web_cycle/main.go
+++ web_cycle/main.go
@@ -2,37 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
 func toRun() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Millisecond * 100)
-		fmt.Println(i)
+	var urls = []string{"https://www.google.com", "https://www.yahoo.com", "https://www.sky.com", "https://www.bbc.co.uk"}
+	for _, url := range urls {
+		fmt.Println("Fetching: " + url)
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			body, err := ioutil.ReadAll(resp.Body)
+			fmt.Println(string(body))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
 	}
 }
 
 func main() {
-
-	// var urls = []string{"https://www.google.com", "https://www.yahoo.com", "https://www.sky.com", "https://www.bbc.co.uk"}
-
 	go toRun()
 
 	var input string
 	fmt.Scanln(&input)
 
-	// for _, url := range urls {
-	// 	fmt.Println("Fetching: " + url)
-	// 	resp, err := http.Get(url)
-	//
-	// 	if err != nil {
-	// 		fmt.Println(err.Error())
-	// 	} else {
-	// 		body, err := ioutil.ReadAll(resp.Body)
-	// 		fmt.Println(string(body))
-	// 		if err != nil {
-	// 			fmt.Println(err)
-	// 		}
-	// 	}
-	// }
 }
