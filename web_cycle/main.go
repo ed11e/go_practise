0185edb89b008e7292f7f9d6d9af18232c9613b6
Marsh, Edward <edward.marsh@sky.uk>
2017-04-26 14:42:04 +0100

    Remove commented out code

--- web_cycle/main.go
+++ web_cycle/main.go
@@ -6,21 +6,6 @@ import (
 	"net/http"
 )
 
-// for _, url := range urls {
-// 	fmt.Println("Fetching: " + url)
-// 	resp, err := http.Get(url)
-//
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	} else {
-// 		body, err := ioutil.ReadAll(resp.Body)
-// 		fmt.Println(string(body))
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 	}
-// }
-
 func main() {
 	var urls = []string{"https://www.google.com", "https://www.yahoo.com", "https://www.sky.com", "https://www.bbc.co.uk"}
 	resultsChan := make(chan string)
