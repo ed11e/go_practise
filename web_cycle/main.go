a1573f0ac229d6df9a490f7b92e5a564c6d29187
Marsh, Edward <edward.marsh@sky.uk>
2017-04-26 14:19:14 +0100

    Add thread loop

--- web_cycle/main.go
+++ web_cycle/main.go
@@ -1,28 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
+import "fmt"
 
 func getUrls() {
 	var urls = []string{"https://www.google.com", "https://www.yahoo.com", "https://www.sky.com", "https://www.bbc.co.uk"}
 	for _, url := range urls {
-		fmt.Println("Fetching: " + url)
-		resp, err := http.Get(url)
+		go func() {
+			fmt.Println(url)
 
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			body, err := ioutil.ReadAll(resp.Body)
-			fmt.Println(string(body))
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
+		}()
 	}
 }
+
+// for _, url := range urls {
+// 	fmt.Println("Fetching: " + url)
+// 	resp, err := http.Get(url)
+//
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	} else {
+// 		body, err := ioutil.ReadAll(resp.Body)
+// 		fmt.Println(string(body))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}
+// }
 
 func main() {
 	go getUrls()
