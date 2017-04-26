63c3b754b7cfddae4a5526ec445598f837c1f022
Marsh, Edward <edward.marsh@sky.uk>
2017-04-26 14:22:14 +0100

    Functions working thread blocking

--- web_cycle/main.go
+++ web_cycle/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func getUrls() {
 	var urls = []string{"https://www.google.com", "https://www.yahoo.com", "https://www.sky.com", "https://www.bbc.co.uk"}
 	for _, url := range urls {
 		go func() {
-			fmt.Println(url)
-
+			fmt.Println("Fetching: " + url)
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				body, err := ioutil.ReadAll(resp.Body)
+				fmt.Println(string(body))
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
 		}()
 	}
 }
